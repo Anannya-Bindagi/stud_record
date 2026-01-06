5db83f71a11027aea184917d10bcc9ca19010592
Anannya <01fe24ica023@kletech.ac.in>
2026-01-06 06:46:58 +0530

    added test_student.py.

---
+++ test_student.py
@@ -0,0 +1,16 @@
+import pytest
+from student import get_student_result
+def test_student_grade_A():
+    name = "anannya"
+    dept = "bca"
+    semester = "3"
+    m1 = 85
+    m2 = 90
+    m3 = 95
+
+    expected_output = (
+        "Name: anannya, Department: bca, Semester: 3, "
+        "Average: 90.00, Grade: S"
+    )
+
+    assert get_student_result(name, dept, semester, m1, m2, m3) == expected_output
